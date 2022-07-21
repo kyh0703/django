ae82345e7d15af2c6c7028c560fc46a540072b1a
Kimyeonho <kyh0703@nate.com>
2022-07-21 16:11:52 +0900

    [Add] Update User DFR

--- shop/authentication/api/views.py
+++ shop/authentication/api/views.py
@@ -1,16 +1,17 @@
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.views import APIView
 
-from .serializers import RegistrationSerializer, LoginSerializer
+from .serializers import RegistrationSerializer, LoginSerializer, UserSerializer
 from .renderers import UserJSONRenderer
 
 
 class RegistrationAPIView(APIView):
     permission_classes = (AllowAny,)
-    serializer_class = RegistrationSerializer
     renderer_classes = (UserJSONRenderer,)
+    serializer_class = RegistrationSerializer
 
     def post(self, request):
         user = request.data
@@ -24,8 +25,8 @@ class RegistrationAPIView(APIView):
 
 class LoginAPIView(APIView):
     permission_classes = (AllowAny,)
-    serializer_class = LoginSerializer
     renderer_classes = (UserJSONRenderer,)
+    serializer_class = LoginSerializer
 
     def post(self, request):
         user = request.data
@@ -33,4 +34,23 @@ class LoginAPIView(APIView):
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
 
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.serializer_class(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, *args, **kwargs):
+        serializer_data = request.data
+        serializer = self.serializer_class(
+            request.user, data=serializer_data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
