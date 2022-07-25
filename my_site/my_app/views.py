1f5423c908a990e1aaf14606508b54ecb3fe85b1
Kimyeonho <kyh0703@nate.com>
2022-07-26 00:06:27 +0900

    [템플릿과 URL]

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -7,11 +7,4 @@ def example_view(request):
 
 
 def variable_view(request):
-    my_var = {
-        "first_name": "ROSALIND",
-        "last_name": "franklin",
-        "some_list": [1, 2, 3],
-        "some_dict": {"inside_key": "inside_value"},
-    }
-
-    return render(request, "my_app/variable.html", context=my_var)
+    return render(request, "my_app/variable.html")
--- my_site/setting/urls.py
+++ my_site/setting/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("my_app/", include("my_app.urls")),
+    path("admin/", admin.site.urls),
 ]
