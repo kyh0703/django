80cf099c282bd8eee42cd3383caf9056e21d5ab2
Kimyeonho <kyh0703@nate.com>
2022-08-07 23:04:01 +0900

    [add form]

---
+++ my_form/cars/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "cars"
+
+urlpatterns = [
+    path("rental_view/", views.rental_review, name="rental_view"),
+    path("thank_you/", views.thank_you, name="thank_you"),
+]
